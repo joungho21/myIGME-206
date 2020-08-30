096a7d7b4c571dbe7e3228809340ca955c079ea8
Joungho Seo <seojouh@gmail.com>
2020-08-29 22:27:21 -0400

    Implemented PE3 for Question Number 5

---
+++ PE3/PE3_Q5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE3_Q5
+{
+    class Program
+    {
+        // PE3 Question 5
+        // Author: Joungho Seo
+        // Purpose: Console Application obtains 4 int values from user and display the product
+        static void Main(string[] args)
+        {
+            int result =1; //Instantiates with 1 so that it will not conflict with the first input
+            for (int i = 0; i <4; i++) { //For loop to get 4 int values
+                Console.WriteLine("Please enter an integer:");
+                result *=Convert.ToInt32(Console.ReadLine()); //Parsing string input to int
+            }
+            Console.WriteLine("The total product of those 4 interger is " + result);
+
+
+        }
+    }
+}
