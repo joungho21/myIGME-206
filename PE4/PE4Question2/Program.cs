0174a35d5ed4eaf072da89c1e83a3998cd92323d
Joungho Seo <seojouh@gmail.com>
2020-08-31 15:02:11 -0400

    Implemented Question 2 from PE 4

---
+++ PE4/PE4Question2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE4Question2
+{
+    class Program
+    {
+        // PE4 Question 2
+        // Author: Joungho Seo
+        // Purpose: 2.	Write a console application that includes the logic from Exercise 1 
+        static void Main(string[] args)
+        {
+            bool loop = true;
+            int num1;
+            int num2;
+            while (loop)
+            {
+                Console.WriteLine("Please enter an integer less than 10:");
+                num1 = Convert.ToInt32(Console.ReadLine()); //Parsing string input to int
+
+                Console.WriteLine("Please enter another integer less than 10:");
+                num2 = Convert.ToInt32(Console.ReadLine()); 
+
+                if (num1 > 10 || num2 > 10) //If any of the input is > 10
+                {
+                    Console.WriteLine("You entered an integer greater than 10, please try again!"); //Goes back to the loop
+                }
+                else
+                {
+                    Console.WriteLine("The Exclusive OR between " + num1 + " and " + num2 + " = " + (num1 ^ num2));
+                    loop = false; //stops the loop
+                }
+            }
+        }
+    }
+}
