ec0757990188b11e482bbc24aff9bc496922360a
Joungho Seo <seojouh@gmail.com>
2020-09-09 23:45:58 -0400

    Implemented PE9 for Questions 2 and 3

---
+++ PE9/PE9Q3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE9Q3
+{
+    class Program
+    {
+        // PE9 Q3
+        // Author: Joungho Seo
+        // Purpose: 3.	Create a delegate function and use it to impersonate the Console.ReadLine() 
+        // Restrictions: Only outputs what was inputted
+        
+        public delegate string myDelegate(); //declare a delegate
+
+        static void Main()
+        {
+            Console.WriteLine("Please enter a message you would like to echo");
+            string output = Readline(); //testing readline method from myDelegate 
+            Console.WriteLine(output);
+        }
+         static string Readline() //readline function for myDelegate signature
+        {
+            return Console.ReadLine();
+        }
+            
+        
+
+        }
+    }
